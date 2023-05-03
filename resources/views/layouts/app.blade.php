01176ae885de578d40d29621ef7a98e6495ddedf
soukainaserh <soukainaserh456@gmail.com>
2023-05-04 00:08:37 +0200

    premeier commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'ECO SALMA') }}</title>
+    <title>{{ config('app.name', 'Bienvenue chez nous') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -22,7 +22,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ route('index_product') }}">
-                    {{ config('app.name', 'ECO SALMA') }}
+                    {{ config('app.name', 'Bienvenue chez nous ') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
